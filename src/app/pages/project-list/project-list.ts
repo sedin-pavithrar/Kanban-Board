b6131947ea0308ef2dcd0acc0c33e41910c7ca2f
sedin-pavithrar <pavithra.r@sedintechnologies.com>
2026-04-27 11:38:50 +0530

    Added project-list

--- src/app/pages/project-list/project-list.ts
+++ src/app/pages/project-list/project-list.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { KanbanStorageService } from '../../core/services/kanban-storage';
 
 @Component({
   selector: 'app-project-list',
-  imports: [],
-  templateUrl: './project-list.html',
-  styleUrl: './project-list.css',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './project-list.component.html',
+  styleUrl: './project-list.component.css'
 })
-export class ProjectList {}
+export class ProjectListComponent {
+  storage = inject(KanbanStorageService);
+  private router = inject(Router);
+
+  projectName = signal('');
+
+  createProject(): void {
+    const name = this.projectName().trim();
+
+    if (!name) return;
+
+    const project = this.storage.createProject(name);
+    this.router.navigate(['/projects', project.id]);
+  }
+
+  openProject(id: string): void {
+    this.router.navigate(['/projects', id]);
+  }
+}
