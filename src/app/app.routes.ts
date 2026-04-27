1dc87398a22e3c041421d0f9de3900c61a2a6c1b
sedin-pavithrar <pavithra.r@sedintechnologies.com>
2026-04-27 12:44:06 +0530

    Changes made in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { OnboardingComponent } from './pages/onboarding/onboarding';
 import { ProjectListComponent } from './pages/project-list/project-list';
 import { KanbanBoardComponent } from './pages/kanban-board/kanban-board';
+import { onboardingGuard, authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', component: OnboardingComponent },
-  { path: 'projects', component: ProjectListComponent },
-  { path: 'projects/:id', component: KanbanBoardComponent }
+  { path: '', component: OnboardingComponent, canActivate: [onboardingGuard] },
+  { path: 'projects', component: ProjectListComponent, canActivate: [authGuard] },
+  { path: 'projects/:id', component: KanbanBoardComponent, canActivate: [authGuard] }
 ];
