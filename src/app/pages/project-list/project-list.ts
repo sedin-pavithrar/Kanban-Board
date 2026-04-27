ae5bc7973e3ea642faa8c57936bc65bfa30a2bf0
sedin-pavithrar <pavithra.r@sedintechnologies.com>
2026-04-27 16:07:51 +0530

    added comments for clarity

--- src/app/pages/project-list/project-list.ts
+++ src/app/pages/project-list/project-list.ts
@@ -1,6 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; // form handle for ngmodel
 import { KanbanStorageService } from '../../core/services/kanban-storage';
 
 @Component({
@@ -12,20 +12,20 @@ import { KanbanStorageService } from '../../core/services/kanban-storage';
 })
 export class ProjectListComponent {
   storage = inject(KanbanStorageService);
-  private router = inject(Router);
+  private router = inject(Router); 
 
-  projectName = signal('');
+  projectName = signal(''); // stores  current value of the project name input field. and it also updates UI 
 
   createProject(): void {
-    const name = this.projectName().trim();
+    const name = this.projectName().trim(); // get input value
 
-    if (!name) return;
+    if (!name) return; //guard clause
 
-    const project = this.storage.createProject(name);
-    this.router.navigate(['/projects', project.id]);
+    const project = this.storage.createProject(name); // { id: '', name: 'project name' }
+    this.router.navigate(['/projects', project.id]); // pge navi
   }
 
   openProject(id: string): void {
     this.router.navigate(['/projects', id]);
-  }
+  } //existing proj open
 }
